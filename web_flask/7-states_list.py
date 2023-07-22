cbd0a84ae67d0f5a9b5880cd84b621a9438d5f1d
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-07-22 20:59:45 +0300

    python flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""
-    Script that starts a Flask web application
-    Must be listening on 0.0.0.0, port 5000
-    Uses storage for fetchig data drom storange engine
-"""
+"""Flask framework"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/state_list', strict_slashes=False)
-def list_states():
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_storage(exception):
-    """Closes current db connection"""
-    storage.close()
+def teardown_db(exception):
+    """ teardown db"""
+    if storage is not None:
+        storage.close()
 
 
-if __name__ == "__main__":
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ list of state ids"""
+    data = storage.all(State)
+    return render_template('7-states_list.html', total=data.values())
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
